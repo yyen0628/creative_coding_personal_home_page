f8a69141f8b83c45c5c6e6da2a9fd7e5044213c9
yyen0628 <ericyen0628@gmail.com>
2024-09-18 09:10:34 -0500

    0918_particle_update

--- particle_system/sketch.js
+++ particle_system/sketch.js
@@ -1,23 +1,29 @@
 let particles = [];
 
 function setup() {
+    
   createCanvas(windowWidth, windowHeight);
-  
-  
+
     let p = new Particle();
     particles.push(p);
 
-  pixelDensity(2);
+  pixelDensity(5);
+  frameRate(30);
 }
 
 function draw() {
-  background(0);
+    background(0);
   
   for(let i = 0; i < particles.length; i ++) {
     let p = particles[i];
     p.update();
     p.show();
+
+    if(particles.length <= 0) {
+        particles.push(new Particle());
+    }
   }
+  console.log(particles.length);
 }
 
 function windowResized() {
@@ -30,9 +36,9 @@ class Particle {
     this.pos = createVector(random(width), random(height));
     this.age = 500;
     this.c = 1;
-    this.vel = createVector(random(-5, 5), random(-5, 5));
-    this.accel = createVector(random(-0.03, 0.03), random(-0.03, 0.03));
-    
+    this.vel = createVector(random(-5, 5), random(-3, 3));
+    this.accel = createVector(random(-0.05, 0.05), random(-0.03, 0.03));
+    this.size = random(30, 50)
   }
   
   update() {
@@ -45,24 +51,29 @@ class Particle {
     if(this.age < 0 || this.age >= 500) {
         this.c *= -1
     }
-    this.color = map(this.age, 500, 0, 255, 0);
-    this.size = map(this.age, 500, 0, 50, 0);
+    this.colorR = map(this.pos.x, width, 0, 255, 0);
+    this.colorG = map(this.pos.y, height, 0, 255, 0);
+    this.colorB = this.vel.x * 50;
+    this.color = map(this.age, 500, 0, 255, 10);
   }
   
   show() {
     noStroke();
-    fill(this.color, random(255), random(255));
+    //fill(this.colorR, this.colorG, this.colorB);
+    fill(255, this.color);
     ellipse(this.pos.x, this.pos.y, this.size);
   }
   
   checkWalls() {
     if(this.pos.x < 0 || this.pos.x > width) {
         this.vel.x *= -1;
+        particles.push(new Particle());
       }
+      
       
       if(this.pos.y < 0 || this.pos.y > height) {
         this.vel.y *= -1;
-        //this.size += 1;
+        particles.length --;
       }
   }
 }
